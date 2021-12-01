ff400db55db7f1b25af1371e0bd73120eeed0170
Ron Valstar <ron@ronvalstar.nl>
2021-12-01 22:45:03 +0100

    added cascading cell updates

--- src/js/index.js
+++ src/js/index.js
@@ -26,42 +26,24 @@ function onInputFileChange(event){
 function onFileReaderLoad(e) {
   const data = e.target.result
   const workbook = XLSX.read(data, {type: 'binary', bookDeps: true})
-  console.log('workbook',workbook) // todo: remove log
+  const hfInstance = getHyperFormulaInstance(workbook)
+
+  console.log('workbook', workbook) // todo: remove log
+  console.log('hfInstance', hfInstance) // todo: remove log
 
   // sjs-F11
-  // sjs-
-
-  const map = Object.entries(workbook.Sheets)
-      .map(([sheetName, sheet])=>{
-        console.log('sheet',sheet) // todo: remove log
-        return Object.entries(sheet)
-            .filter(([key])=>key[0]!=='!')
-            .map(([cellName, wbvalue])=>{
-              const {t:type,n:number,v:value,f:fnc} = wbvalue
-              const fullName = sheetName+'.'+cellName
-              const formula = fnc&&parseFormula(fnc)||{formula:fnc}
-              return {type,number,value,sheetName,cellName,fullName,...formula}
-            })
-      })
-      .reduce((acc, entries)=>{
-        entries.forEach(cell=>acc[cell.fullName]=cell)
-        return acc
-      }, {})
 
   output.innerHTML = workbook.SheetNames
     .map(sheet => `<div data-sheet="${sheet}">${getHTML(workbook, sheet)}</div>`)
     .join('')
 
-  output.addEventListener('click', onClickOutput.bind(null, workbook, map))
+  output.addEventListener('click', onClickOutput.bind(null, workbook, hfInstance))
+  hfInstance.on('valuesUpdated', onHyperFormulaValuesUpdated.bind(null, workbook, output))
+}
 
-  //////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////
-
-  console.log('cellToXY',cellToXY('AA1')) // todo: remove log
-
+function getHyperFormulaInstance(workbook) {
   const hfSheets = Object.entries(workbook.Sheets, {})
       .map(([sheetName, sheet])=>{
-        console.log('sheet',sheet) // todo: remove log
         const colRows = []
         Object.entries(sheet)
             .filter(([key])=>key[0]!=='!')
@@ -77,10 +59,8 @@ function onFileReaderLoad(e) {
       })
       .reduce((acc, [sheetName, entries])=>((acc[sheetName] = entries), acc), {})
 
-  console.log('hfSheets', hfSheets) // todo: remove log
   const hfInstance = HyperFormula.buildFromSheets(hfSheets, {licenseKey: 'gpl-v3'}) // MIT
-  console.log('hfInstance',hfInstance) // todo: remove log
-  console.log(' ',hfInstance.getCellValue({ col: 2, row: 5, sheet: 0 }))
+  return hfInstance
 }
 
 function cellToXY(cellName) {
@@ -90,16 +70,30 @@ function cellToXY(cellName) {
   return [x, y]
 }
 
+function xyToCell(x, y) {
+  return numeralToString(x)+y.toString()
+}
+
 function stringToNumeral(string) {
   return string.split('').reverse().reduce((acc,s,i)=>acc+s.charCodeAt(0)-65+i*26,0)
 }
+
+function numeralToString(numeral) {
+  let s = ''
+  while (numeral > 0) {
+    const index = (numeral - 1) % 26
+    s = String.fromCharCode(65 + index) + s
+    numeral = (numeral - index)/26 | 0
+  }
+  return s
+}
+[0,1,25,26,27,111,1E4].forEach(i=>console.log(`numeralToString(${i})`,numeralToString(i),stringToNumeral(numeralToString(i)))) // todo: remove log
 
 function getBase64(file) {
   const reader = new FileReader()
   reader.readAsDataURL(file)
   reader.addEventListener('error', console.log.bind(console))
 }
-
 
 function overwriteLog(){
   const {console} = window
@@ -115,60 +109,36 @@ function overwriteLog(){
   window.onerror = console.log.bind(console)
 }
 
-
-function onClickOutput(workbook, map, e) {
+function onClickOutput(workbook, hfInstance, e) {
   const {target, target: {id, dataset: {t:type, v:value}}} = e
   if (type==='n') {
     const wrapper = target.closest('[data-sheet]')
     const sheetName = wrapper.dataset.sheet
     const cellName = id.split(/\-/).pop()
-    const fullName = sheetName+'.'+cellName
     //
-    console.log('click',fullName,map[fullName])//{sheetName,cell,sheet,formulae,obj}) // todo: remove log
+    const sheet = workbook.SheetNames.indexOf(sheetName)
     //
+    const [col, row] = cellToXY(cellName)
+    const cellAddress = {col, row, sheet}
+    //
+    const cellValue = hfInstance.getCellValue(cellAddress)
+    const cellFormula = hfInstance.getCellFormula(cellAddress)
+    const canSetContents = hfInstance.isItPossibleToSetCellContents(cellAddress)
+    //
+    if (cellFormula===undefined&&canSetContents) {
+      hfInstance.setCellContents(cellAddress, cellValue+1)
+    }
   }
 }
 
-function parseFormula(formula){
-  console.log('formulaValue',formula) // todo: remove log
-  const [, func, paramses] = formula.match(/^([A-Z^(]+)\(([^)]*)\)$/)||[,,]
-  const params = paramses?.split(/,/g)
-  //console.log(func,paramses)
-  const paramValues = params?.map(parseParam)
-  //
-  return {formula,func,params,paramValues}
-}
-
-// function parseFormula(cellFormula){
-//   const [,cell,formulaValue] = cellFormula.match(/^([A-Z]+\d+)=(.*)$/)
-//   const string = formulaValue.match(/\'.+/)&&formulaValue.replace(/^\'|\'$/g, '')
-//   const number = parseNumber(formulaValue)
-//   //
-//   const [, func, paramses] = formulaValue.match(/^([^(]+)\(([^)]*)\)$/)||[,,]
-//   const params = paramses?.split(/,/g)
-//   const paramValues = params?.map(parseParam)
-//   //
-//   return {cell,formula:formulaValue,cellFormula,string,number,func,params,paramValues}
-// }
-
-function parseParam(paramValue){
-  const [,fromX,fromY,toX,toY] = paramValue?.match(/^([A-Z]+)(\d+):([A-Z]+)(\d+)$/)
-  const range = getRange(fromX,fromY,toX,toY)
-  const number = parseNumber(paramValue)
-  const isNumber = number!==undefined
-  return isNumber?number:(range&&paramValue)
-}
-
-const undef = (()=>{})()
-function parseNumber(string){
-  const float = parseFloat(string)
-  return float.toString()===string?float:undef
-}
-
-function getRange(fromX,fromY,toX,toY){
-  const validParams = fromX&&fromY&&toX&&toY
-  // todo make range
-  return validParams?[]:undef
+function onHyperFormulaValuesUpdated(workbook, parent, changed){
+  changed.forEach(change=>{
+    const {address: {col, row, sheet}, newValue} = change
+    const cellName = xyToCell(col+1, row+1)
+    const sheetName = workbook.SheetNames[sheet]
+    const cellElment = parent.querySelector(`[data-sheet="${sheetName}"] [id="sjs-${cellName}"]`)
+    cellElment.textContent = newValue
+  })
 }
 
 function getHTML(workbook, sheet){
